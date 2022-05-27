db1dfc33e4acca45d791a91031b00efe18994251
Aluno <aluno@LI.LOCAL>
2022-05-27 20:30:03 -0300

    Tanque nav mesh
    2

---
+++ Assets/Scripts/AgentManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+
+public class AgentManager : MonoBehaviour
+{
+    public NavMeshAgent agent;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                    agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
